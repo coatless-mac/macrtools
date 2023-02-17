c4242da14c5cbe1b1367b65119529398520c579b
balamut2@illinois.edu <james.balamuta@gmail.com>
2023-02-16 21:17:36 -0800

    Ensure the correct installation paths are used for both x86_64 and arm64.
    h/t @s-u for catching the lack of support on `arm64`.

--- R/recipes.R
+++ R/recipes.R
@@ -175,32 +175,47 @@ recipes_binary_install = function(
     urls <- up(url, os.arch, db[need, "Binary"])
 
     if (action == "install") {
+
+        # Check if we're using sudo & have a password
         if (sudo && is.null(password))
             password = askpass::askpass()
+
+        # Determine the correct installation path based on arch type
+        if( grepl("arm64", os.arch, fixed = TRUE)) {
+            strip_levels = 3
+            install_directory = shQuote("/opt/R/arm64/")
+        } else if (grepl("x86_64", os.arch, fixed = TRUE)) {
+            strip_levels = 2
+            install_directory = shQuote("/usr/local/")
+        } else {
+            stop("Arch type not recognized. Please make sure you are on either an `arm64` or `x86_64` system.")
+        }
+
         for (binary_url in urls) {
-            cat("Downloading + installing ", binary_url, "...\n")
+            binary_file_name = basename(binary_url)
+            cat("Downloading + installing ", binary_file_name, "...\n")
 
             # Three step procedure:
             cat("Downloading tar: ", binary_url, "...\n")
 
             # Step One:Download the package into the temporary directory
-            save_location = file.path(tempdir(), basename(binary_url))
+            save_location = file.path(tempdir(), binary_file_name)
             utils::download.file(
                 binary_url,
                 save_location
             )
 
-            cat("Installing: ", basename(binary_url), " into /usr/local/ ...\n")
+            cat("Installing: ", binary_file_name, " into ", install_directory ," ...\n")
 
             # Step Two: Install the package using tar with stdin redirect
-            cmd = paste0("tar fxj ", save_location," -C /usr/local/ --strip 2")
+            cmd = paste0("tar fxj ", save_location," -C ", install_directory, " --strip ", strip_levels)
             status = shell_execute(cmd, sudo = sudo, password = password)
 
             # Verify installation is okay:
             if (status < 0)
                 stop("Failed to install from ", save_location)
 
-            cat("Removing temporary tar file: ", basename(binary_url), "...\n")
+            cat("\nRemoving temporary tar file: ", binary_file_name, "...\n")
 
             # Step Three: Remove the tar package
             unlink(save_location)
