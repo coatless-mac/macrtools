95374a3635ddcbb2b1e230b540a634c2e102f576
balamut2@illinois.edu <james.balamuta@gmail.com>
2023-03-07 12:38:17 -0800

    Add a force password option and verify_status template

--- R/utils.R
+++ R/utils.R
@@ -145,3 +145,29 @@ print.cli = function(x, ...) {
     cat("Output:\n", paste(x$output, collapse = "\n"), "\n")
     cat("Status:\n", paste(x$status, collapse = "\n"), "\n")
 }
+
+# Display warning ----
+
+verify_status = function(status, program, url, type = c("uninstall", "install")) {
+    type = match.arg(type)
+
+    if(isFALSE(status)) {
+        cat("We were not able to ", type, program, " ...\n")
+        if(!missing(url)) {
+            cat("Please try to manually ", type, "using: ..\n")
+            cat(url, "\n")
+        }
+        return(invisible(FALSE))
+    }
+}
+
+
+force_password = function(supplied_password) {
+    entered_password = supplied_password
+    if(is.null(entered_password)) {
+        cat("Please enter your password in the prompt that is appearing to continue ...\n\n")
+        entered_password = askpass::askpass()
+    }
+
+    entered_password
+}
