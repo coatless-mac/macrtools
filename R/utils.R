b007c1e5a20f7caa6cac4bcc1a7dca979ec80d3d
James J Balamuta <james.balamuta@gmail.com>
2023-05-28 14:39:14 -0700

    Document version extraction

--- R/utils.R
+++ R/utils.R
@@ -76,12 +76,17 @@ is_x86_64 = function() {
 is_r_version = function(target_version, compare_major_minor = TRUE) {
 
     minor_value = if (compare_major_minor) {
+        # If x.y.z, this retrieves y
         strsplit(R.version$minor, ".", fixed = TRUE)[[1]][1]
     } else {
+        # If x.y.z, this retrieves y.z
         R.version$minor
     }
 
+    # Build the version string of x.y or x.y.z
     version_string = paste(R.version$major, minor_value, sep = ".")
+
+    # Check for equality.
     return(version_string == target_version)
 }
 
