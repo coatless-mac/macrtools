8403a3fb6b3d4f87a79951ba4e35a6b9ea609dd6
James J Balamuta <james.balamuta@gmail.com>
2023-12-07 00:06:33 -0800

    Move xcode-select to a separate file.

---
+++ R/xcode-select.R
@@ -0,0 +1,30 @@
+#' Interface with `xcode-select` Shell Commands
+#'
+#' Trigger `xcode-select` commands from within _R_
+#'
+#' @param args Flag arguments to pass to `xcode-select`
+#' @export
+#' @rdname xcode-select
+xcode_select = function(args) {
+    out = sys::exec_internal("xcode-select", args = args, error = FALSE)
+
+    structure(
+        list(
+            output = sys::as_text(out$stdout),
+            status = out$status
+        ),
+        class = c("xcodeselect", "cli")
+    )
+}
+
+#' @export
+#' @rdname xcode-select
+xcode_select_path = function() {
+    xcode_select("--print-path")
+}
+
+#' @export
+#' @rdname xcode-select
+xcode_select_version = function() {
+    xcode_select("--version")
+}
